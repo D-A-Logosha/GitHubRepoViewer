5137778daa2ef035361f40706797ba7689ec93e0
D.A.Logosha <D.A.Logosha@gmail.com>
2024-07-05 13:49:53 +0700

    Implement layout for AuthFragment

--- app/src/main/java/com/example/githubrepoviewer/ui/auth/AuthFragment.kt
+++ app/src/main/java/com/example/githubrepoviewer/ui/auth/AuthFragment.kt
@@ -1,7 +1,26 @@
 package com.example.githubrepoviewer.ui.auth
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.githubrepoviewer.databinding.FragmentAuthBinding
 
 class AuthFragment: Fragment() {
-    // TODO:
+
+    private var _binding: FragmentAuthBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentAuthBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
