2f5b2b5d0a76415904dd9a0949e0c399aba2aa06
D.A.Logosha <D.A.Logosha@gmail.com>
2024-07-05 22:11:55 +0700

    Implement logic for AuthFragment

--- app/src/main/java/com/example/githubrepoviewer/ui/auth/AuthFragment.kt
+++ app/src/main/java/com/example/githubrepoviewer/ui/auth/AuthFragment.kt
@@ -1,16 +1,31 @@
 package com.example.githubrepoviewer.ui.auth
 
+import android.content.Context
+import android.content.res.ColorStateList
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.example.githubrepoviewer.R
 import com.example.githubrepoviewer.databinding.FragmentAuthBinding
 
-class AuthFragment: Fragment() {
+class AuthFragment : Fragment() {
 
     private var _binding: FragmentAuthBinding? = null
     private val binding get() = _binding!!
+
+    private val viewModel: AuthViewModel by viewModels()
+
+    private val inputMethodManager by lazy {
+        requireContext().getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
@@ -19,8 +34,106 @@ class AuthFragment: Fragment() {
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.apply {
+            root.setFocusableInTouchMode(true)
+            root.setOnClickListener {
+                if (etTokenInput.hasFocus()) etTokenInput.clearFocus()
+            }
+            etTokenInput.nextFocusForwardId = btnSignIn.id
+            btnSignIn.nextFocusForwardId = etTokenInput.id
+            etTokenInput.clearFocus()
+        }
+        viewModel.token.observe(viewLifecycleOwner) {
+            if (viewModel.token.value != binding.etTokenInput.text.toString()) binding.etTokenInput.setText(
+                viewModel.token.value
+            )
+        }
+
+        viewModel.state.observe(viewLifecycleOwner) {
+            when (viewModel.state.value) {
+                AuthViewModel.State.Idle -> idleState()
+                is AuthViewModel.State.TokenInput -> inputState((viewModel.state.value as AuthViewModel.State.TokenInput).isTooShort)
+                is AuthViewModel.State.InvalidInput -> invalidInputState((viewModel.state.value as AuthViewModel.State.InvalidInput).reason)
+                AuthViewModel.State.Loading -> loadingState()
+                null -> {}
+            }
+        }
+
+        binding.etTokenInput.onFocusChangeListener = View.OnFocusChangeListener { _, hasFocus ->
+            val token = binding.etTokenInput.text.toString()
+            viewModel.onTokenChanged(token, hasFocus)
+            if (hasFocus) binding.etTokenInput.showKeyboard()
+            else binding.etTokenInput.hideKeyboard()
+        }
+
+        binding.etTokenInput.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+                val token = s.toString()
+                viewModel.onTokenChanged(token)
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+        })
+
+        binding.btnSignIn.setOnClickListener {
+            if (binding.etTokenInput.hasFocus()) binding.etTokenInput.clearFocus()
+            viewModel.onSignButtonPressed()
+        }
+    }
+
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun idleState() {
+        binding.tvTokenLabel.visibility = View.INVISIBLE
+        binding.etTokenInput.backgroundTintList =
+            ColorStateList.valueOf(ContextCompat.getColor(requireContext(), R.color.grey))
+        binding.tvTokenError.visibility = View.INVISIBLE
+    }
+
+    private fun inputState(isTooShort: Boolean) {
+        binding.tvTokenLabel.visibility = View.VISIBLE
+        if (isTooShort) {
+            val color = ContextCompat.getColor(requireContext(), R.color.secondary)
+            binding.tvTokenLabel.setTextColor(color)
+            binding.etTokenInput.backgroundTintList = ColorStateList.valueOf(color)
+        } else {
+            binding.tvTokenLabel.setTextColor(
+                ContextCompat.getColor(requireContext(), R.color.white_hint)
+            )
+            binding.etTokenInput.backgroundTintList =
+                ColorStateList.valueOf(ContextCompat.getColor(requireContext(), R.color.grey))
+        }
+        binding.tvTokenError.visibility = View.INVISIBLE
+    }
+
+    private fun loadingState() {
+        TODO()
+    }
+
+    private fun invalidInputState(reason: String) {
+        val color = ContextCompat.getColor(requireContext(), R.color.error)
+        binding.tvTokenLabel.visibility = View.VISIBLE
+        binding.tvTokenLabel.setTextColor(color)
+        binding.etTokenInput.backgroundTintList =
+            ColorStateList.valueOf(color)
+        binding.tvTokenError.visibility = View.VISIBLE
+        binding.tvTokenError.text =
+            getString(
+                R.string.invalid_token,
+                reason.takeIf { it.isNotBlank() }?.let { ": $it" } ?: "")
+    }
+
+    private fun EditText.showKeyboard() {
+        inputMethodManager.showSoftInput(this, InputMethodManager.SHOW_IMPLICIT)
+    }
+
+    private fun EditText.hideKeyboard() {
+        inputMethodManager.hideSoftInputFromWindow(this.windowToken, 0)
     }
 }
--- app/src/main/java/com/example/githubrepoviewer/ui/auth/AuthViewModel.kt
+++ app/src/main/java/com/example/githubrepoviewer/ui/auth/AuthViewModel.kt
@@ -1,22 +1,45 @@
 package com.example.githubrepoviewer.ui.auth
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
 
-class AuthViewModel {
-    val token: MutableLiveData<String> = TODO()
-    val state: LiveData<State> = TODO()
-    val actions: Flow<Action> = TODO()
+class AuthViewModel : ViewModel() {
 
+    val token: MutableLiveData<String> = MutableLiveData("")
+
+    private var _state = MutableLiveData<State>(State.Idle)
+    val state: LiveData<State> = _state
+
+    private var _actions = MutableSharedFlow<Action>()
+    val actions: Flow<Action> = _actions.asSharedFlow()
+
+    private val tokenPattern = Regex("^[A-Za-z0-9_]{0,255}$")
+    private var tokenChangeJob: Job? = null
+
+    private var i = 0
     fun onSignButtonPressed() {
-        // TODO:
+        viewModelScope.launch(Dispatchers.IO) {
+            Log.d("d", "${i++}")
+        }
     }
 
     sealed interface State {
-        object Idle : State
-        object Loading : State
+        data object Idle : State
+        data object Loading : State
         data class InvalidInput(val reason: String) : State
+        data class TokenInput(val isTooShort: Boolean) : State
     }
 
     sealed interface Action {
@@ -24,5 +47,72 @@ class AuthViewModel {
         object RouteToMain : Action
     }
 
-    // TODO:
+    private data class CachedInput(
+        var token: String = "",
+        var hasFocus: Boolean = false,
+        val mutex: Mutex = Mutex(),
+    )
+
+    private val cachedInput = CachedInput()
+
+    fun onTokenChanged(token: String, hasFocus: Boolean = true) {
+        this.token.value = token
+        viewModelScope.launch {
+            cachedInput.mutex.withLock {
+                cachedInput.token = token
+                cachedInput.hasFocus = hasFocus
+            }
+            if (tokenChangeJob == null)
+                performValidation(token, hasFocus)
+        }
+    }
+
+    private fun performValidation(token: String, hasFocus: Boolean = true) {
+        tokenChangeJob = viewModelScope.launch {
+            _state.value = when (validateToken(token)) {
+                ValidationResult.VALID -> State.TokenInput(isTooShort = false)
+
+                ValidationResult.INVALID_FORMAT -> State.InvalidInput("Invalid format")
+
+                ValidationResult.EMPTY -> State.Idle
+
+                ValidationResult.TOO_SHORT -> if (hasFocus) State.TokenInput(isTooShort = true)
+                else State.InvalidInput("Too short")
+
+                ValidationResult.TOO_LONG -> State.InvalidInput("Too long")
+            }
+            delay(VALIDATION_DELAY_MILLIS)
+        }
+        viewModelScope.launch {
+            val tokenChangeJobLocal = tokenChangeJob
+            tokenChangeJobLocal?.join()
+            cachedInput.mutex.withLock {
+                if (cachedInput.token != token || cachedInput.hasFocus != hasFocus) {
+                    performValidation(cachedInput.token, cachedInput.hasFocus)
+                } else tokenChangeJob = null
+            }
+        }
+    }
+
+    private fun validateToken(token: String): ValidationResult {
+        return when {
+            token.isEmpty() -> ValidationResult.EMPTY
+            token.length > 255 -> ValidationResult.TOO_LONG
+            !tokenPattern.matches(token) -> ValidationResult.INVALID_FORMAT
+            token.length < 40 -> ValidationResult.TOO_SHORT
+            else -> ValidationResult.VALID
+        }
+    }
+
+    enum class ValidationResult {
+        VALID,
+        INVALID_FORMAT,
+        EMPTY,
+        TOO_SHORT,
+        TOO_LONG,
+    }
+
+    private companion object {
+        const val VALIDATION_DELAY_MILLIS = 250L
+    }
 }
